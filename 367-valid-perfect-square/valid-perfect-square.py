72582a4107ef86970f3dd378cba7975b49279717
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2024-03-16 14:46:21 +0500

    Time: 30 ms (86.65%) | Memory: 16.5 MB (42.09%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,11 +1,30 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        i = 1
-        # iterate untill reach to the perfect square(i * i) of 'num'
-        while i * i <= num:
-            if i * i == num:
+
+        # brute force
+
+        # i = 1
+        # # iterate untill reach to the perfect square(i * i) of 'num'
+        # while i * i <= num:
+        #     if i * i == num:
+        #         return True
+        #     i = i + 1
+        # return False
+
+
+        # Binary search
+
+        left, right = 1, num
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            if square == num:
                 return True
-            i = i + 1
+            elif square < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
         return False
 
             
