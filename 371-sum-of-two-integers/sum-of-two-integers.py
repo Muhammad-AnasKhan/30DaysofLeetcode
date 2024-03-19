e8c2504f7a290f4f1387f8b298e3fd5f1459793d
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2024-03-19 14:55:05 +0500

    Time: 20 ms (9.05%) | Memory: 11.8 MB (10.12%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,5 +1,9 @@
-class Solution:
-    def getSum(self, a: int, b: int) -> int:
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
         return sum([a,b])
-
         
