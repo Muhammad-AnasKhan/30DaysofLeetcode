3d8a1c00759553d3a87d85af7986bb16e4653dba
Muhammad Ans Khan <91449185+Muhammad-AnasKhan@users.noreply.github.com>
2024-03-26 12:26:58 +0500

    Time: 24 ms (60.49%) | Memory: 21.3 MB (73.07%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//  Step 1: Finding the Middle Node
+// Step 2: Reversing the Second Half
+// Step 3: Reordering the List
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) {
+            return;
+        }
+
+        ListNode* middle = midNode(head);
+        ListNode* newHead = middle->next;
+        middle->next = nullptr;
+
+        newHead = reverseLinkedList(newHead);
+
+        ListNode* c1 = head;
+        ListNode* c2 = newHead;
+        ListNode* f1 = nullptr;
+        ListNode* f2 = nullptr;
+
+        while (c1 && c2) {
+            // Backup
+            f1 = c1->next;
+            f2 = c2->next;
+
+            // Linking
+            c1->next = c2;
+            c2->next = f1;
+
+            // Move
+            c1 = f1;
+            c2 = f2;
+        }
+
+    }
+    private:
+    ListNode* midNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* reverseLinkedList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* forw = nullptr;
+
+        while (curr) {
+            forw = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+};
